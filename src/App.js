efcbec65fa27af35dc043f4d083e2b6548623d97
Darshikpp <darshikpramod@gmail.com>
2023-07-21 12:55:41 +0530

    babylon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, ColorGradient, Scene, Material, Color3 } from "@babylonjs/core";
+import SceneComponent from "./SceneComponent"; // uses above component in same directory
+// import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let box;
+// let sphere;
 
-export default App;
+const onSceneReady = (scene) => {
+  // This creates and positions a free camera (non-mesh)
+  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+  // This targets the camera to scene origin
+  camera.setTarget(Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
+
+  // Our built-in 'box' shape.
+  box = MeshBuilder.CreateBox("box", { size: 4}, scene);
+  Material.diffuseColor = (new Color3(1, 0, 1))
+
+  //create a sphere
+
+  // sphere = MeshBuilder.CreateSphere('sphere',{segments:5,},scene)
+  
+ 
+
+
+  // Move the box upward 1/2 its height
+  box.position.y = 1;
+
+  // Our built-in 'ground' shape.
+  MeshBuilder.CreateGround("ground", { width: 8, height: 9 }, scene);
+};
+
+/**
+ * Will run on every frame render.  We are spinning the box on y-axis.
+ */
+const onRender = (scene) => {
+  if (box !== undefined) {
+    const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 50;
+    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  }
+};
+
+export default () => (
+  <div>
+
+    <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" />
+  </div>
+);
